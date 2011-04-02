97484c97b17f5859edcafe02d8101bfaf5324f74
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-02 12:07:04 +0400

    написал перефигачивалку в digits
    и очень смеялся на кратных 10.
    отличный вопрос для собеседования кстати - где тут ошибка

--- 20/compute.lua
+++ 20/compute.lua
@@ -10,4 +10,21 @@ function factsum(n)
     end
 end
 
-factsum(50)
+function getdigits(num)
+    local iterator = num
+    local res = {}
+    while iterator > 9 do
+        local degree = 10 ^ (math.ceil(math.log10(iterator)-1))
+        local digit = math.floor(iterator/degree)
+        table.insert(res, digit)
+        iterator = iterator - degree * digit
+    end
+    table.insert(res,iterator)
+    return res
+end
+
+num = 10000
+print(num)
+res = getdigits(num)
+for i,v in pairs(res) do print(i,v) end
+
