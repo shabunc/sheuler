771d2cb76db10288bcb0daa87a7218fb618003e5
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-09 00:27:34 +0400

    переписал с зайцем и черепашкой
    на ассерте не врёт

--- 35/compute.lua
+++ 35/compute.lua
@@ -25,34 +25,22 @@ function allprimes()
     end
 end
 
-function problem(n)
-    co = coroutine.wrap(allprimes)
-    while true do
-       local p, is_prime  = co()
-       if is_prime then
-           if (p > n) then
-                break
-           end
-       end
-    end
-end
 
 function shift(n)
     local len = math.floor(math.log10(n + 1))
     local deg = 10^len
     local biggest = math.floor(n / deg) 
-    return (n - deg * math.floor(n / deg)) * 10 + biggest
+    return (n - deg * math.floor(n / deg)) * 10 + biggest, biggest
 end
 
 function shift_iterator(n) 
     return coroutine.wrap(function() 
         local turtle, hair = n, n
-        local count = 0
         while true do
-            count = count + 1
-            turtle = shift(turtle)
+            local first_digit
+            turtle, first_digit  = shift(turtle)
             hair = shift(shift(hair))
-            coroutine.yield(turtle)
+            coroutine.yield(turtle, first_digit)
             if turtle == hair then
                 break
             end
@@ -60,12 +48,41 @@ function shift_iterator(n)
     end)
 end
 
-
-local next_shift  = shift_iterator(123123)
-while true do
-    local n = next_shift()
-    print(n)
-    if not n then
-        break
+function prime_is_cycle(p) 
+    local get_next = shift_iterator(p)
+    local count = 0
+    while true do
+        local n_shifted, first_digit = get_next()
+        if not n_shifted then
+            break
+        end
+        if first_digit == p then
+            return 1
+        end
+        if first_digit % 2 == 0 or first_digit == 5 then
+            return 0
+        end
+        if not primes.primes[n_shifted] then
+            return 0
+        end
+        count = count + 1
     end
+    return count
 end
+
+function problem(n)
+    co = coroutine.wrap(allprimes)
+    local total = 1 --two already is counted
+    while true do
+       local p, is_prime  = co()
+       if is_prime then
+           if (p > n) then
+                break
+           end
+           total = total + prime_is_cycle(p)
+       end
+    end
+    print(total)
+end
+
+problem(100)
