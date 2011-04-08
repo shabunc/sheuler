36229c7d8b9de92031b4759a4dfaaa4e9472bcba
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-08 08:40:48 +0400

    параметризовал

--- 9/compute.lua
+++ 9/compute.lua
@@ -5,18 +5,18 @@ function test(a)
     return remainder == 0
 end
 
-function problem()
-    for a = 1, 333 do
-        if test(a) then
-            b = 1000 - a - ((a*a)/(1000 - a))  
+function problem(n)
+    for a = 1, n/3 do
+        if (a*a) % (n - a) == 0 then
+            b = n - a - ((a*a)/(n - a))  
             b = b * 0.5
-            assert((a + b + math.sqrt(a*a + b*b)) == 1000)
+            assert((a + b + math.sqrt(a*a + b*b)) == n)
             -- поверим на слово авторам проекта, и на первом найденном остановимся
-            return a, b, (1000 - a - b)
+            return a, b, (n - a - b)
         end
     end
 end
 
 
-a, b, c = problem()
+a, b, c = problem(10000)
 print(a,b,c, a*b*c)
