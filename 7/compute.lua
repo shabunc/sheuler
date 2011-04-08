9e21c24e91f12e94d2a41c208f4d1e99dfa6206b
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-08 10:36:08 +0400

    bruteforce
    9 секунд, ответ 104743

---
+++ 7/compute.lua
@@ -0,0 +1,33 @@
+#!/usr/bin/env lua
+
+local primes = {
+ ["primes"] = {[2] = 1},
+ ["card"] = 1
+}
+
+function is_prime(n) 
+    for p, v in pairs(primes.primes) do
+        if (n % p == 0) then
+            return false
+        end
+    end
+    primes.primes[n] = 1
+    primes.card = primes.card + 1
+    return true
+end
+
+function problem(n)
+    --[[
+    ]]
+    local num = 3
+    while true do
+        is_prime(num)
+        if primes.card == n then
+            return num, primes.card
+        end
+        num = num + 2
+    end
+end
+
+num = problem(10001)
+print(num)
