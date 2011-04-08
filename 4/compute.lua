c8929286871dd583415b846c3f8d208e26e67d78
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-08 07:02:04 +0400

    попытка брутфорса
    на это раз неудачная.
    кроме того, по каким-то пока загадочным для меня причинам
    не пробрасывается значение при multiple_value
    это меня беспокоиь

---
+++ 4/compute.lua
@@ -0,0 +1,45 @@
+#!/usr/bin/env lua
+
+function getdigits(num)
+    local iterator = num
+    local res = {}
+    for i = math.ceil(math.log10(num+1)) - 1,0,-1 do
+        local digit =  math.floor(iterator/(10^i))
+        table.insert(res, digit)
+        iterator = iterator - digit * 10^i
+    end
+    return res
+end
+
+function is_palindrome(num)
+    local digits = getdigits(num)
+    for j = 1, math.ceil(#digits/2) do
+        if digits[j] ~=  digits[#digits - j + 1] then
+            return false
+        end
+    end
+    return true, num
+end
+
+function test_ab(a,b) 
+    return is_palindrome(a*b)
+end
+
+function problem(from, to) 
+    local a,b
+    local pal
+    for j = from, to do
+        for i = j, to do
+            local is_pal
+            is_pal, pal  = test_ab(j, i)
+            if is_pal then
+                a, b = j, i
+            end
+        end
+    end
+    print(pal)
+    return a, b, pal
+end
+
+a, b, palindrome = problem(100, 999)
+print(a, b, a*b)
