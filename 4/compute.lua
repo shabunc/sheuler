2de79b7aeb79fa7068928e74658da4c46ea90bb1
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-08 07:39:30 +0400

    поднапрягшись, понимаем, что палиндром должен делиться на 11
    тут никаких сомнений быть не может)
    это из признака делимости Паскаля следует

--- 4/compute.lua
+++ 4/compute.lua
@@ -29,8 +29,9 @@ function problem(from, to)
     local a,b
     local pal
     local res = 0
-    for j = from, to do
-        for i = j, to do
+    local pal_divisor = 11
+    for j = math.floor(to/pal_divisor) * pal_divisor, from, -pal_divisor do
+        for i = to, from, -1 do
             local is_pal
             is_pal, pal  = test_ab(j, i)
             if is_pal and pal > res then
