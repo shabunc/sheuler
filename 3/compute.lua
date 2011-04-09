8f478f13acca2ac77640460c39c565e7f25a49f6
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-10 02:53:48 +0400

    дорос до того, чтобы аккуратно переносить в модуль

--- 3/compute.lua
+++ 3/compute.lua
@@ -1,31 +1,9 @@
 #!/usr/bin/env lua
 
-local primes = {
+package.path = package.path .. ";/Users/shabunc/mine/euler/modules/?.lua"
+require("numeric")
 
-}
-
-function divisors(n) 
-    local res = {}
-    for j = 2, math.sqrt(n) do
-        local skip = false
-        for p in pairs(primes) do
-            if j % p == 0 then
-                skip = true
-                res[p] = res[p] + 1
-                break
-            end
-        end
-        if not skip then
-            if n % j == 0 then
-                res[j] = 1
-                primes[j] = 1
-            end
-        end
-    end
-    return res
-end
-
-d = divisors(600851475143)
+d = numeric.divisors(600851475143)
 for k,v in pairs(d) do
     print(k,v)
 end
---
+++ modules/numeric.lua
@@ -0,0 +1,29 @@
+numeric = {}
+
+local primes = {
+
+}
+
+function divisors(n) 
+    local res = {}
+    for j = 2, math.sqrt(n) do
+        local skip = false
+        for p in pairs(primes) do
+            if j % p == 0 then
+                skip = true
+                res[p] = res[p] + 1
+                break
+            end
+        end
+        if not skip then
+            if n % j == 0 then
+                res[j] = 1
+                primes[j] = 1
+            end
+        end
+    end
+    return res
+end
+
+numeric.divisors = divisors
+return numeric
