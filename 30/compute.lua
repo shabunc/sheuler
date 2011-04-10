b85e157a7808b6b4f2f40e7cbb05a7250ef1ef72
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-10 21:10:52 +0400

    перечитал формулировку задачи, и понял что просто не всё досчитал

--- 30/compute.lua
+++ 30/compute.lua
@@ -30,9 +30,9 @@ function ascending_iterator(t, n)
     end)
 end
 
-function problem30(n)
+function seqs_for_n(n, pow)
     local iterator = ascending_iterator({0,1,2,3,4,5,6,7,8,9}, n)
-    local pow_sum = function(a,b) return a + b^n end
+    local pow_sum = function(a,b) return a + b^pow end
     local total = 0
     while true do
         local seq = iterator()
@@ -44,13 +44,18 @@ function problem30(n)
         local sorted = array.copy(digits)
         table.sort(sorted)
         if array.are_equal(sorted, seq) then
-            print(sum)
             total = total + sum
         end
     end
-    print("=====")
-    print(total)
     return total
 end
 
-problem30(5)
+function problem30()
+    local total = 0
+    for j = 2, 9 do
+        total = total + seqs_for_n(j, 5)
+    end
+    print(total)
+end
+
+problem30()
