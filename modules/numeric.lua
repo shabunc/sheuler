cec7765a12dabcb859e412788c16c89228e389d8
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-10 16:16:42 +0400

    исправил неверно работающую конверртацию
    затесалась опечатка

--- modules/numeric.lua
+++ modules/numeric.lua
@@ -39,7 +39,7 @@ end
 local function num2digits(n)
     local res = {}
     local len = math.floor(math.log10(n+1))
-    for j = len - 1, 0, -1  do
+    for j = len, 0, -1  do
         local digit =  math.floor(n/(10^j))
         table.insert(res, digit)
         n = n - digit * 10^j
