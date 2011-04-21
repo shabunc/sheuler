2a51e5dc02c08811909583c8f0b2cc6606d5fe54
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-22 00:58:22 +0400

    подготовительные работы перед взрывом

--- 111/compute.lua
+++ 111/compute.lua
@@ -27,3 +27,25 @@ function stat_for_seq(t)
     return res
 end
 
+function problem111(n)
+    local start = {1}
+    for j = 2, n do
+        table.insert(start, 0)
+    end
+    local it = numeric.integer_iterator(start)
+    while true do
+        local seq = it()
+        it()
+        if #seq > n then
+            break
+        end
+        if seq[#seq] ~= 5 then
+            local num_seq = numeric.digits2num(seq)
+            if numeric.is_prime(num_seq) then
+                print(num_seq)
+            end
+        end
+    end
+end
+
+problem111(4)
