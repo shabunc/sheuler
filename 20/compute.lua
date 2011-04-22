9ebbd849f1d895d7fc620597458568c710b0a84f
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-22 10:10:27 +0400

    пока мозги отдыхают от 111-й, решил вернуться к старенькой 20-й
    не-а (((

--- 20/compute.lua
+++ 20/compute.lua
@@ -1,105 +1,35 @@
 #!/usr/bin/env lua
+package.path = package.path .. ";../modules/?.lua"
 
+require("numeric")
+require("array")
 require("pascal")
 
-function getdigits(num)
-    local iterator = num
+function brute(n) 
+    return (array.reduce(numeric.num2digits(numeric.factorial(n)), function(a, b) return a + b end))
+end
+
+function skip_tens(n) 
+    local two_awaiting
+    local five_awaiting
     local res = {}
-    for i = math.ceil(math.log10(num+1)) - 1,0,-1 do
-        local digit =  math.floor(iterator/(10^i))
-        table.insert(res, digit)
-        iterator = iterator - digit * 10^i
+    for j = 1, n do
+        table.insert(res, j)
+        if j % 5 == 0 then
+            five_awaiting = j
+        end
+        if j % 2 == 0 then
+            two_awaiting = j
+        end
+        if five_awaiting and two_awaiting then
+            res[five_awaiting] = res[five_awaiting] / 5
+            res[two_awaiting] = res[two_awaiting] / 2
+            five_awaiting, two_awaiting = false, false
+        end
     end
-    return res
+    local prod = array.reduce(res, function(a, b) return a * b end, 1)
+    return array.reduce(numeric.num2digits(prod), function(a, b) return a + b end)
 end
 
-function reduce(t, func)
-    local res = t[1]
-    for i=2,#t do
-        res = func(res, t[i])
-    end
-    return res
-end
-
-function total(t)
-    return reduce(t, function(a,b) return a+b end)
-end
-
-function factorby(n, p) 
-    local degree = 0
-    local iterator = n
-    while iterator % p == 0 do
-        degree = degree + 1
-        iterator = iterator / p
-    end
-    return degree , iterator
-end
-
-function problem_equivalent(n) 
-    local fives, remainder = factorby(n, 5)
-    local twos, remainder = factorby(remainder, 2)
-    local degree
-    if fives > twos then
-        degree = fives - twos
-        remainder = (5^degree) * remainder
-    else
-        degree = twos - fives
-        remainder = (2^degree) * remainder
-    end
-    return remainder, degree
-end
-
-function problem_func(n) 
-    return total(getdigits(n))
-end
-
-function fact(n) 
-    if (n==1) then 
-        return 1
-    else
-        return n * fact(n-1)
-    end
-end
-
-function problem_effective(n) 
-    local twos = 0
-    local fives = 0
-    local factor = 1
-    for i=2,n do
-        local p2,remainder = factorby(i, 2)
-        twos = twos + p2
-        local p5,remainder = factorby(remainder, 5)
-        fives = fives + p5
-        factor = factor * remainder
-    end
-    if fives > twos then
-        factor = factor * (5^(fives - twos))
-    else
-        factor = factor * (2^(twos - fives))
-    end
-    return problem_func(factor)
-end
-
-function problem_bruteforce(n) 
-    local fact = 1
-    for i=2,n do
-        fact = fact*i
-    end
-    return problem_func(fact)
-end
-
-function problem_log(n) 
-    
-end
-
-num = tonumber(arg[1])
---print(table.concat(pascal.divisibility_rule(num), " "))
-print(pascal.divisible({3,8,2,2,0},13))
-print(pascal.divisible({1,7,4,4,2},51))
-print(pascal.divisible({1,7,4,4,2},19))
---[[
-for j=1,num do
-    local sum = problem_effective(j)
-    print(j, problem_bruteforce(j), sum, fact(j))
-end
-]]
+local n = 100
+print(skip_tens(n), brute(n))
