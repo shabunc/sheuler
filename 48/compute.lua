836f7e08320f9822c2d2fca26c50f625d83a89a6
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-24 02:12:32 +0400

    совсем обнаглел и полез считать в лоб )))
    со своим-то доморощенными бигинтом)
    надо переписать times и попробовать снова.
    хотя - хотя в принципе тоже реально - считать будет несколько часов)

---
+++ 48/compute.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env lua
+package.path = package.path .. ";../modules/?.lua"
+
+require("numeric")
+require("bigint")
+
+function pow(n, m)
+    local big = bigint:new(numeric.num2digits(n))
+    for j = 1, m - 1 do
+        big  = big:times(n)
+    end
+    return big
+end
+
+function problem48(n) 
+    local total = bigint:new{1}
+    for j = 2, n do
+        print(j, pow(j,j))
+    end
+    return total
+end 
+
+problem48(1000)
