680159d11129be28af8fb9a4af950a5a74a2347b
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-23 23:53:20 +0400

    Лешил добить 20-ю задачу
    и не вижу никакого пособа кроме как написать свой bigint
    он сейчас, по правде говоря, адский)))

---
+++ modules/bigint.lua
@@ -0,0 +1,70 @@
+require("array")
+--shame on me)))
+require("numeric")
+
+
+local function __tostring(self)
+    return table.concat(self.num)
+end
+
+local function bigint_new(num, base)
+    local res = {}
+    res.num = num
+    res.base = base or 10
+    local mt = {
+        __index = bigint,
+        __tostring = __tostring
+    }
+    setmetatable(res, mt)
+    return res
+end
+
+bigint = {}
+
+
+function bigint:new(num, base) 
+    return bigint_new(num, base)
+end
+
+function bigint:inc()
+    local base = self.base or 10
+    local res = array.copy(self.num)
+    local max_digit = base - 1
+    local i = #res - 1
+    if res[#res] ~= max_digit then
+        res[#res] = res[#res] + 1
+    else
+        res[#res] = 0
+        while res[i] == max_digit do
+            res[i] = 0
+            i = i - 1
+        end
+        if i == 0 then
+            table.insert(res, 1, 1)
+        else
+            res[i] = res[i] + 1
+        end
+    end
+    return bigint:new(res, base)
+end
+
+function bigint:add(n)
+    local res = bigint:new(self.num, self.base)
+    for j = 1, n do
+        res = res:inc()
+    end
+    return res
+end
+
+function bigint:times(n)
+    local res = bigint:new(self.num, self.base)
+    local m = numeric.digits2num(self.num)
+    for j = 1, n - 1 do
+        for k = 1, m do
+            res = res:inc()
+        end
+    end
+    return res
+end
+
+return bigint
