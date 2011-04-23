b9a9f74e16887317fc9e0cb8f2a0b1792e24d585
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-23 19:41:23 +0400

    thread
    скромный модуль со скромным названием)

---
+++ modules/thread.lua
@@ -0,0 +1,29 @@
+--[[
+    local iterator = thread.wrap(something)
+    while iterator.alive() do
+        local item = iterator()
+        ...
+    end
+]]
+
+thread = {}
+
+local function wrap(func)
+    local co = coroutine.create(func)
+    local callfunc = function(...)
+        return select(2, coroutine.resume(co,...))
+    end
+    local mt = {
+        __call = callfunc
+    }
+    local res = {
+        alive = function() 
+            return coroutine.status(co) ~= "dead"
+        end
+    }
+    setmetatable(res, mt)
+    return res
+end
+
+thread.wrap = wrap
+return thread
