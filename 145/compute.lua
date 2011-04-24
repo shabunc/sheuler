8b79a29ab4675605516d398f6ffd8e360e19bbf6
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-24 11:41:29 +0400

    брутфорс - наглый  и долгий)

--- 145/compute.lua
+++ 145/compute.lua
@@ -20,11 +20,51 @@ function is_reversible(big)
     return res
 end 
 
+local function inc(digit, n, base)
+    base = base or 10
+    if (digit + n) >= base then
+        return (digit + n - base), 1
+    else
+        return (digit + n), 0
+    end
+end
+
+function bigint:add_if_reversible(b)
+    if self[self:len()] == 0 then
+        return false
+    end
+    local a = self:copy()
+    if b:len() > a:len() then
+        a, b = b, a
+    end
+    local i = a:len()
+    local dlen = a:len() - b:len()
+    local rem = 0
+    while true do
+        a[i], rem = inc(a[i], (b[i - dlen] or 0) + rem, self.base)
+        if a[i] % 2 == 0 then
+            return false
+        end
+        i = i - 1
+        if i == 0 then
+            if rem > 0 then
+                if rem % 2 == 0 then
+                    return false
+                end
+                table.insert(a.num, 1, rem)
+            end
+            break
+        end
+    end
+    return a
+end
+
 function problem145(len)
     local total  = 0
     local big = bigint:new{1} 
     while big:len() < len do
-        if is_reversible(big) then
+        local test = big:add_if_reversible(big:revert())
+        if test then
             print(big)
             total = total + 1
         end
