ba7bc82f3797903a5e97146d061c098a7d4404be
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-04-25 11:14:30 +0400

    подготовка перед боем два

--- 37/compute.lua
+++ 37/compute.lua
@@ -6,23 +6,25 @@ require("array")
 
 function step(t) 
     local res = {}
-    for i, v in pairs(t) do
-        for _, j in ipairs({1,3,5,7,9}) do
-            local a = array.copy(v)
-            table.insert(a, 1, j)
-            local left_n  = numeric.digits2num(a)
-            if numeric.is_prime(left_n) then
-                print(#a, table.concat(a))
-                for _, k in ipairs({1,3,7,9}) do
-                    local b = array.copy(v)
-                    table.insert(b, k)
-                    local right_n = numeric.digits2num(b)
-                    if numeric.is_prime(right_n) then
-                        table.insert(a, k)
-                        if numeric.is_prime(numeric.digits2num(a)) then
-                            table.insert(res, a)
-                        end
+    for _, v in ipairs(t) do
+        for _, left_digit in ipairs({1,2,3,4,5,6,7,8,9}) do
+            local from_left = array.copy(v)
+            table.insert(from_left, 1, left_digit)
+            local num_left = numeric.digits2num(from_left)
+            if numeric.is_prime(num_left) then
+                local from_right = array.copy(from_left)
+                local found = true
+                while #from_right > 1 do
+                    table.remove(from_right, #from_right)
+                    local num_right = numeric.digits2num(from_right)
+                    if not numeric.is_prime(num_right) then
+                        found = false
+                        break
                     end
+                end
+                if found then
+                    print(num_left)
+                    table.insert(res, from_left)
                 end
             end
         end
@@ -32,18 +34,13 @@ end
 
 
 function problem37()
-    local iterator = {{}}
-    local iterator = {{},{2},{3},{5},{7}}
+    local iterator = {{2}, {3}, {5}, {7}}
     local count = 1
     while true do 
-        if count > 4 then
+        if count > 6 then
             break
         end
         iterator = step(iterator)
-        for _, v in ipairs(iterator) do
-            print(table.concat(v))
-        end
-        print("=======")
         count = count + 1
     end
 end
