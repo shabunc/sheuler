9b7ad06d6f3dbf0d4c0343752a5dbe6493b02baa
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-05-05 01:59:56 +0400

    работающий вариант
    без доказательства, что найденный локальный минимум  глобыльный правда.

--- 44/compute.lua
+++ 44/compute.lua
@@ -8,6 +8,7 @@ function problem44(f)
     local chain = {}
     local nums = {}
     local n = 1
+    local min = math.huge
     while true do
         table.insert(nums, f(n))
         local b = nums[#nums]
@@ -17,20 +18,29 @@ function problem44(f)
            if found > 0 then
                 chain[b] = a
                 chain[a] = b - a
-                if chain[b-a] then
-                    print(b, a, b - a, chain[b-a])
+                if chain[b - a] and chain[chain[a] - chain[b - a]] then
+                    local D = chain[chain[a] - chain[b - a]]
+                    print(b, a, b - a, D)
+                    if D < min then
+                        min = D
+                    end
                 end
            end
         end
-        if n > 10 then
+        if n > 3000 then
             break
         end
         n = n + 1
     end
+    print("MIN", min)
 end
 
 function penta(n)
     return n*(3*n - 1) * 0.5
+end
+
+function triangle(n)
+    return n * (n + 1) * 0.5
 end
 
 problem44(penta)
