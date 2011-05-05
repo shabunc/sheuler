25e92c1a8b906922da0a6dfd744bfd4c7a5cee28
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-05-05 22:59:45 +0400

    8602840 7042750 1560090 5482660
    ;)

--- 44/compute.lua
+++ 44/compute.lua
@@ -4,35 +4,40 @@ package.path = package.path .. ";../modules/?.lua"
 require("numeric")
 require("array")
 
+
+function key(a,b)
+    local res = a.." "..b
+    return res
+end
+
 function problem44(f)
     local chain = {}
     local nums = {}
     local n = 1
     local min = math.huge
     while true do
+        print(n)
         table.insert(nums, f(n))
         local b = nums[#nums]
         for j = #nums - 1, 1, -1 do
            local a = nums[j] 
            local found = array.index_of(nums, b - a)
            if found > 0 then
-                chain[b] = a
-                chain[a] = b - a
-                if chain[b - a] and chain[chain[a] - chain[b - a]] then
-                    local D = chain[chain[a] - chain[b - a]]
-                    print(b, a, b - a, D)
-                    if D < min then
-                        min = D
-                    end
-                end
+               if chain[key(a, b - a)] then
+                    print(b, a, b - a, chain[key(a, b - a)])
+                    return {b, a, b - a, chain[key(a, b - a)]}
+               end
+               chain[key(b, a)] = b - a
            end
         end
         if n > 3000 then
             break
         end
+        for i, v  in pairs(chain) do
+            --print(i)
+        end
         n = n + 1
     end
-    print("MIN", min)
 end
 
 function penta(n)
