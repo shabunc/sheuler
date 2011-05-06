dfd6a0b41928aad3daa339e3da838b43a539b17c
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-05-07 02:42:59 +0400

    подготовка к бою

---
+++ 34/compute.lua
@@ -0,0 +1,29 @@
+#!/usr/bin/env lua
+package.path = package.path .. ";../modules/?.lua"
+
+require("numeric")
+require("array")
+
+function is_curious(n, func) 
+    if n == 1 or n == 2 then
+        return false
+    end
+    local digs = numeric.digits(n)
+    local nfsum = array.reduce(digs, function(a, b) return a + func(b) end)
+    return nfsum == n, nfsum
+end
+
+function problem34()
+    local n = 1
+    while true do
+        local curious, actual_sum = is_curious(n, numeric.factorial)
+        if curious then
+            print(n)
+        end
+        n = n + 1
+    end
+end
+
+assert(is_curious(145, numeric.factorial) == true)
+
+problem34()
