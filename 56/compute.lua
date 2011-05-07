8945b95826b2dfe97a87a5e51c95eb61f93b1531
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-05-07 11:18:19 +0400

    972 ;)

--- 56/compute.lua
+++ 56/compute.lua
@@ -3,19 +3,22 @@ package.path = package.path .. ";/Users/shabunc/mine/euler/modules/?.lua"
 
 require("numeric")
 require("array")
+require("bignum")
 
-function problem56(n) 
+function problem56(n, m) 
     local max = 0
     for a = 1, n do
-        for b = 1, n do
-            local sum = array.reduce(numeric.num2digits(a^b),function(a,b)return a + b end)
+        for b = 1, m do
+            local biga = bignum(numeric.digits(a)):pow(b)
+            local sum = array.reduce(biga, function(a,b)return a + b end)
             if sum > max then
-                print(sum, a^b, a, b)
+                print("new max", a, b, "=>", sum)
                 max = sum
             end
         end
     end
+    print("MAX", max)
     return max
 end
 
-print(problem56(10,10))
+print(problem56(100, 100))
