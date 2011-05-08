161392948647919790b8bee2b5646fb96ee78976
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-05-09 01:25:50 +0400

    level 2, level 2 )))

--- 62/compute.lua
+++ 62/compute.lua
@@ -9,7 +9,6 @@ function is_cube(n)
     return math.ceil(n^(1/3))^3 == n
 end
 
-local CACHE = {}
 
 function find_some(n, func)
     local digs = numeric.digits(n)
@@ -18,6 +17,7 @@ function find_some(n, func)
     local res = {}
     while true do
         local seq = it()
+        --print(seq)
         if not seq then
             break
         end
@@ -31,21 +31,36 @@ function find_some(n, func)
     return res
 end
 
+local CACHE = {}
+
 function problem62(max)
     local n = 100 
     local excluded = {}
     while true do
         local cube = n^3
+        local digs = numeric.digits(cube)
+        table.sort(digs)
+        local key = table.concat(digs,"")
         print(n, cube)
-        local found = find_some(cube, is_cube)
-        if #found == max then
-            print(table.concat(found, " "))
-            return found, n
+        if CACHE[key] then
+            table.insert(CACHE[key], cube)
+            if #CACHE[key] == max then
+                print(table.concat(CACHE[key]," "))
+                return n
+                --[[
+                local found = find_some(tonumber(key), is_cube)
+                print(table.concat(found," "))
+                return found, n
+                ]]
+            end
+        else
+            CACHE[key] = {cube}
         end
         n = n + 1
     end
 end
 
-assert(table.concat(find_some(41063625, is_cube)," ") == "41063625 56623104 66430125")
+--assert(table.concat(find_some(41063625, is_cube)," ") == "41063625 56623104 66430125")
 
+--print(table.concat(find_some(1432197595648, is_cube)))
 problem62(5)
