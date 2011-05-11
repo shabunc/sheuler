1bad79bedd041bec8eeb677983f37961d0a77662
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-05-12 02:41:22 +0400

    проделана огромная и бессмысленная работа )

--- 54/compute.lua
+++ 54/compute.lua
@@ -51,11 +51,15 @@ function Player:hasStraightFlush()
 end
 
 function Player:hasFourOfAKind()
-    return self.cluster[1][2] == 4
+    return self.cluster[1][2] >= 4
+end
+
+function Player:hasThreeOfAKind()
+    return self.cluster[1][2] >= 3
 end
 
 function Player:hasFullHouse()
-    return self.cluster[1][2] == 3 and cluster[2][2] == 2
+    return self.cluster[1][2] >= 3 and self.cluster[2][2] >= 2
 end
 
 
@@ -70,7 +74,7 @@ end
 
 function Player:hasStraight()
     for j = 2, #self.cards do
-        if (self.cards[j] - self.cards[j - 1]) ~= 1 then
+        if (self.cards[j].value - self.cards[j - 1].value) ~= 1 then
             return false
         end
     end
@@ -78,17 +82,57 @@ function Player:hasStraight()
 end
 
 function Player:hasTwoPairs()
-    return self.cluster[1][2] == 2 and self.cluster[2][2] == 2
+    return self.cluster[1][2] >= 2 and self.cluster[2][2] >= 2
 end
 
 function Player:hasOnePair()
-    return self.cluster[1][2] == 2
+    return self.cluster[1][2] >= 2
 end
 
 function Player:highestCard()
     return self.cards[#self.cards] 
 end
 
+function Player:getRank() 
+    local rank = 0
+    if self:hasOnePair() then
+        rank = rank + 1
+    end
+
+    if self:hasTwoPairs() then
+        rank = rank + 1
+    end
+    
+    if self:hasThreeOfAKind() then
+        rank = rank + 1
+    end
+
+    if self:hasStraight() then
+        rank = rank + 1
+    end
+
+    if self:hasFlush() then
+        rank = rank + 1
+    end
+
+    if self:hasFullHouse() then
+        rank = rank + 1
+    end
+
+    if self:hasFourOfAKind() then
+        rank = rank + 1
+    end
+
+    if self:hasStraightFlush() then
+        rank = rank + 1
+    end
+
+    if self:hasRoyalFlush() then
+        rank = rank + 1
+    end
+
+    return rank
+end
 
 Game = class:new()
 function Game:init(player1, player2, data)
@@ -107,16 +151,52 @@ function Game:init(player1, player2, data)
 
     player1:sort()
     player2:sort()
+    
+    self.player1 = player1
+    self.player2 = player2
 end
 
 function Game:findWinner() 
-    if player1:hasRoyalFlush() then
-        return player1
+    local player1 = self.player1
+    local player2 = self.player2
+
+    local rank1 = player1:getRank()
+    local rank2 = player2:getRank()
+    print(rank1, rank2)
+
+    local winner
+    if rank1 > rank2 then
+        winner = player1
+    elseif rank1 < rank2 then
+        winner = player2
+    else
+        winner = self:playerWithHighestCard()
     end
-    if player2:hasRoyalFlush() then
+
+    return winner
+end
+
+function Game:isFirstPlayer(player)
+    return self.player1 == player
+end
+
+function Game:isSecondPlayer(player)
+    return self.player2 == player
+end
+
+function Game:playerWithHighestCard()
+    local player1 = self.player1
+    local player2 = self.player2
+
+    if player2.cards[#player2.cards].value > player1.cards[#player1.cards].value then
         return player2
     end
+    return player1
 end
 
 local game = Game(Player(), Player(), "5H 5C 6S 7S KD 2C 3S 8S 8D TD")
-
+if game:isFirstPlayer(game:findWinner())  then
+    print("PLAYER1")
+else
+    print("PLAYER2")
+end
