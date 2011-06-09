81eb41395308bb675e6cff210a3812e82d9b980a
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-10 01:15:22 +0400

    nextdig

--- 80/compute.lua
+++ 80/compute.lua
@@ -3,4 +3,22 @@ package.path = package.path .. ";../modules/?.lua"
 
 require("numeric")
 require("array")
+require("bignum")
+
+function nextdig(head, lim)
+    local dig
+    for j = 0, 9 do
+        local num = bignum(head)
+        table.insert(num, j)
+        local test = array(num:mul(num), 1, #lim)
+        if test ~= lim then
+            dig = j - 1
+            break
+        end
+    end
+    print(dig)
+    return dig
+end
+
+nextdig(bignum{1}, array{1})
 
