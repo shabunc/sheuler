5b1e3dd1742c56d75c0c1adc3c8f9aaab8584d08
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-09 23:06:55 +0400

    второй подход к снаряду

--- 72/compute.lua
+++ 72/compute.lua
@@ -4,20 +4,29 @@ package.path = package.path .. ";/Users/shabunc/mine/euler/modules/?.lua"
 require("numeric")
 require("array")
 
-function problem72(d)
+function brute72(d)
     local lookup = {}
     local total = 0
     for b = d, 2, -1 do
        for a = 1, b - 1 do
             if numeric.are_rel_primes(a,b) then
                 total = total + 1
-                print(a, b, numeric.are_rel_primes(a, b))
+                --print(a, b, numeric.are_rel_primes(a, b))
             end
        end
     end
     return total
 end
 
+function problem72(n)
+    total = 0
+    for d = 2, n do
+        print(d)
+        total = total + numeric.totient(d)
+    end
+    return total
+end
+
 assert(problem72(8), 21)
 
-print(problem72(10^6))
+print("TOTAL", problem72(10^6))
