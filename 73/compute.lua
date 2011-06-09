e7fe69f1681837238113194a2d50ca4f9632088f
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-10 00:16:30 +0400

    7295372

--- 73/compute.lua
+++ 73/compute.lua
@@ -31,32 +31,18 @@ function get_fractions(d, filter)
 end
 
 function get_all_fractions(max, filter)
-    local res = {}
+    local res = 0
     for n = max, 2, -1 do
        local ns = get_fractions(n, filter) 
        print(n, #ns)
-       for j = 1, #ns do
-            table.insert(res, ns[j])
-       end
+       res = res  + #ns
     end
-    table.sort(res, function(a, b)
-        return a.a * b.b  < b.a * a.b
-    end)
-    setmetatable(res, {
-        __tostring = function() 
-            local s = ""
-            for j = 1, #res do
-                s = s .. " " .. tostring(res[j])
-            end
-            return s
-        end
-    })
     return res
 end
 
 function problem73(max)
     local all = get_all_fractions(max, function(a,b) return a > b/3 and a < b/2 end)
-    res = #all
+    res = all
     --[[
     local from = array.index_of(all, function(i, v) return v.a == 1 and v.b  == 3 end)
     local to = array.index_of(all, function(i, v) return v.a == 1 and v.b  == 2 end)
