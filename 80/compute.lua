be43b2b61413c2d0f3443cfa89e1c5292145f236
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-10 01:37:26 +0400

    близко, но не близко-близко

--- 80/compute.lua
+++ 80/compute.lua
@@ -7,18 +7,32 @@ require("bignum")
 
 function nextdig(head, lim)
     local dig
+    local prevnum
     for j = 0, 9 do
         local num = bignum(head)
         table.insert(num, j)
         local test = array(num:mul(num), 1, #lim)
-        if test ~= lim then
-            dig = j - 1
+        if test == lim then
             break
         end
+        prevnum = num
     end
-    print(dig)
-    return dig
+    return prevnum, prevnum[#prevnum]
 end
 
-nextdig(bignum{1}, array{1})
+function root(n, precision)
+    local lim = array(numeric.digits(n))
+    local head = array(numeric.digits(math.floor(math.sqrt(n))))
+    print(lim, head)
+    local dig
+    for j = 1, precision do
+       head, dig = nextdig(head, lim) 
+    end
+    print(head:mul(head))
+    table.remove(head, 1)
+    print(head)
+    local total = head:reduce(function(a, b) return a + b end)
+    print(total)
+end
 
+print(root(3,100))
