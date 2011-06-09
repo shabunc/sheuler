89b384d5c50058f6f8145e1355ffde3b0f8e5323
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-10 00:04:40 +0400

    filter чтобы сразу отсеивать

--- 73/compute.lua
+++ 73/compute.lua
@@ -17,21 +17,24 @@ function fraction:__tostring()
     return "" .. self.a .. "/" .. self.b
 end
 
-function get_fractions(d)
+function get_fractions(d, filter)
     local res = {}
+    local filter = filter or function() return true end
     for a = d - 1, 1, -1 do
         if numeric.are_rel_primes(a, d) then
-            table.insert(res, fraction(a, d))
+            if filter(a, d) then
+                table.insert(res, fraction(a, d))
+            end
         end
     end
     return res
 end
 
-function get_all_fractions(max)
+function get_all_fractions(max, filter)
     local res = {}
     for n = max, 2, -1 do
-       local ns = get_fractions(n) 
-       print(n)
+       local ns = get_fractions(n, filter) 
+       print(n, #ns)
        for j = 1, #ns do
             table.insert(res, ns[j])
        end
@@ -52,10 +55,18 @@ function get_all_fractions(max)
 end
 
 function problem73(max)
-    local all = get_all_fractions(max)
-    print(all)
+    local all = get_all_fractions(max, function(a,b) return a > b/3 and a < b/2 end)
+    res = #all
+    --[[
+    local from = array.index_of(all, function(i, v) return v.a == 1 and v.b  == 3 end)
+    local to = array.index_of(all, function(i, v) return v.a == 1 and v.b  == 2 end)
+    local res = to - from - 1
+    ]]
+    return res
 end
 
 
-problem73(12000)
+assert(problem73(8) == 3)
+
+print(problem73(12000))
 
