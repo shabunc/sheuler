20f69b53c509ed9170a7b43a9bf3e3ea34481c33
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-15 08:13:50 +0400

    генерю сначала кубы

--- 342/compute.lua
+++ 342/compute.lua
@@ -4,15 +4,33 @@ package.path = package.path .. ";../modules/?.lua"
 require("numeric")
 require("array")
 
+function findcubes(max)
+    print("fetching cubes")
+    local res = {}
+    for j = 1, max do
+        res[j*j*j] = j
+    end 
+    print("done")
+    return res
+end
+MAX = 10^6
+CUBES = findcubes(MAX)
+
+function is_cube(n)
+    return CUBES[n]
+end
+    
 function hypo(n) 
+    total = 0
     for j = 1, n do 
         local phi2 = j * numeric.totient(j)
-        for i = 2, n do
-            if i * i * i == phi2 then
-                print(j, phi2, i)
-            end
+        if is_cube(phi2) then
+            total = total + j
+            print(j, phi2, is_cube(phi2))
         end
     end
+    print("TOTAL", total)
+    return total
 end
 
-hypo(10 ^ 5)
+hypo(MAX)
