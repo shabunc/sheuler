370411e5d0f248ec24f8043ec32b51edcd59a80c
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-16 09:09:00 +0400

    продолжается неумолимая но бессмысленна хаскелизация мира

---
+++ 27/compute.hs
@@ -0,0 +1,27 @@
+--import Control.Monad
+import Data.List
+
+
+divisors :: Integer -> [Integer] -> Integer -> [Integer]
+divisors 1 _ _  = []
+divisors n divs from  
+        | n == from  = nub $ insert from divs
+        | n `mod` from == 0 = divisors (n `div` from) (nub $ insert from divs) from
+        | otherwise = divisors n divs (if from == 2 then 3 else from + 2)
+
+divs :: Integer -> [Integer]
+divs n = divisors n [] 2
+
+is_prime :: Integer -> Bool
+is_prime n = let divs = divisors n [] 2 in length(divs) == 1 && (divs!!0 == n)
+
+poly :: [Integer] -> Integer -> Integer
+poly ks n = sum $ map (\a -> fst a * snd a) (zip ks (iterate (\a -> n * a) 1))
+
+chain :: [Integer] -> [Integer]
+chain ks = takeWhile is_prime [ poly ks n | n <- [0 .. ]]
+
+main :: IO()
+main = do
+    --print $ length $ filter is_prime [1 .. 1000]
+    print $ length $ chain [41, 1, 1]
--- 75/compute.hs
+++ 75/compute.hs
@@ -2,12 +2,12 @@
 import Data.List
 
 triangles :: Integer -> [(Integer, Integer, Integer)]
-triangles p = [(a, b, c) | a <- [1 .. p - 2], b <- [1 .. p - 2], c <- [1 .. p - 2], a < b, b < c, a + b > c, a^2 + b^2 == c^2, a + b + c == p] 
+triangles p = [(a, b, c) | a <- [1 .. p - 2], b <- [a .. p - 2 - a], c <- [p - a - b], b < c, a + b > c, a^2 + b^2 == c^2] 
 
 problem75 n = filter (\a -> length(triangles a) == 1) [3 .. n]
 
 main :: IO()
 main = do
-    print $ length $ problem75 150
+    print $ problem75 1500
     --print $ problem342(10^5)
     -- print $ sum $ filter (\n -> is_cube $ totient2 n) [1..10^10]
