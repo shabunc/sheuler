2e5409968283062b9fac890cc5011b9600fdd70c
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-16 11:46:28 +0400

    нулевое приближение

---
+++ 87/compute.lua
@@ -0,0 +1,35 @@
+#!/usr/bin/env lua
+package.path = package.path .. ";../modules/?.lua"
+
+require("numeric")
+require("array")
+
+function func(a, b, c) 
+    return numeric.prime(a)^2 + numeric.prime(b)^3 +numeric.prime(c)^4
+end
+
+function find_largest(max, func)
+    for n = 3, math.huge do
+        if (numeric.prime(n) ^ 2 + numeric.prime(n-1)^3 + numeric.prime(n-2)^4) > max then
+            return n
+        end
+    end
+end
+
+function find_all(max)
+    local pmax = find_largest(max) 
+    local total = 0
+    for j = 1, pmax do
+        for i = 1, pmax do
+            for k = 1, pmax do
+                if func(i,j,k) <= max then
+                    total = total + 1
+                end
+            end
+        end
+    end
+    return total
+end
+
+assert(find_all(50) == 4)
+print("TOTAL", find_all(50 * 10^6))
