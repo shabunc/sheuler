e970a383f73e68c98dbfb5c6a1c53ecd300ebbd2
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-17 02:51:42 +0400

    всё готово к бою

--- 303/compute.lua
+++ 303/compute.lua
@@ -3,6 +3,7 @@ package.path = package.path .. ";../modules/?.lua"
 
 require("numeric")
 require("array")
+require("bignum")
 
 function f(n) 
     if n % 99 == 0 then
@@ -17,12 +18,39 @@ function f(n)
     if n % 989 == 0 then
         return 11212202012, 11212202012 / n
     end
-    for j = 1, math.huge do 
+    if n % 999 == 0 then
+    end
+    for j = 10^7, math.huge do 
         local m = j * n
         local digs = numeric.digits(m)
         if array.every(digs, function(d) return d <3 end) then
-            print(m, j)
             return m, j
+        end
+    end
+end
+
+function fdiad(n) 
+    if n % 99 == 0 then
+        return 1122222222, 1122222222 / n
+    end
+    if n % 813 == 0 then
+        return 1111122222, 1111122222 / n
+    end
+    if n % 909 == 0 then
+        return 10122222222, 10122222222 / n
+    end
+    if n % 989 == 0 then
+        return 11212202012, 11212202012 / n
+    end
+    if n % 999 == 0 then
+        return 112222222222222, 112222222222222 / n
+    end
+    local b = bignum({0},3) 
+    while true do
+        b = b:add(bignum({1},3))
+        local m = numeric.number(b)
+        if m % n == 0 then
+            return m/n, n
         end
     end
 end
@@ -31,15 +59,11 @@ function ff(n)
     local total = 0
     for j = 1, n do
         print(j)
-        local _, k = f(j)
+        local _, k = fdiad(j)
         total = total + k
     end
     return total
 end
 
-print(f(999))
 
---[[
-assert(ff(100) == 11363107)
 print("TOTAL", ff(10000))
-]]
