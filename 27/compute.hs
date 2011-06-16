4cfb27428375ffc912a5ca081d77074ec1762561
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-16 10:14:30 +0400

    первая задача, решённая на хаскеле
    но - НО! - главный язык всё равно lua

--- 27/compute.hs
+++ 27/compute.hs
@@ -1,8 +1,10 @@
 --import Control.Monad
 import Data.List
+import Data.Function
 
 
 divisors :: Integer -> [Integer] -> Integer -> [Integer]
+divisors 0 _ _  = []
 divisors 1 _ _  = []
 divisors n divs from  
         | n == from  = nub $ insert from divs
@@ -13,18 +15,18 @@ divs :: Integer -> [Integer]
 divs n = divisors n [] 2
 
 is_prime :: Integer -> Bool
-is_prime -n = n
 is_prime n = let divs = divisors n [] 2 in length(divs) == 1 && (divs!!0 == n)
 
 poly :: [Integer] -> Integer -> Integer
 poly ks n = sum $ map (\a -> fst a * snd a) (zip ks (iterate (\a -> n * a) 1))
 
 chain :: [Integer] -> [Integer]
-chain ks = takeWhile is_prime [ poly ks n | n <- [0 .. ]]
+chain ks = takeWhile (\p -> is_prime(abs(p)))[ poly ks n | n <- [0 .. ]]
 
-problem27 n = [(a, b) | a <- [-n .. n], b <- [-n .. n]]
+
+problem27 n = sort [(length $ chain [a,b,1], a, b) | a <- [-n .. n], b <- [-n .. n]]
 
 main :: IO()
 main = do
     --print $ length $ filter is_prime [1 .. 1000]
-    print $ problem27 1
+    print $ problem27 1000
