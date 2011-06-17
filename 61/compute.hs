6c42b0a604e1f0d24b76cc07878c545f3675b617
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-18 03:27:26 +0400

    немножко получше

--- 61/compute.hs
+++ 61/compute.hs
@@ -14,12 +14,10 @@ are_siblings a b
     | a!!2 == b!!0 && a!!3 == b!!1 = True
     | otherwise = False
 
-digs n = map (digitToInt) $ show n
-
-is_pretty_match l = (length $ filter (\a -> are_siblings (fst a) (snd a)) $ zip l (tail $ cycle l)) == length l
+is_pretty_match l = all (\a -> are_siblings (fst a) (snd a)) $ zip l (tail $ cycle l)
 
 main :: IO()
 main = do
-    --print $ [[a,b,c] | a <- figs 3, b <- figs 4, c <- figs 5, are_siblings (digs a) (digs b), are_siblings (digs b) (digs c), are_siblings (digs c) (digs a)]
-    print $ is_pretty_match ["8128", "2882", "8281"]
+    print $ [[a,b] | a <- figs 3, b <- figs 4, is_pretty_match [show a, show b]]
+    -- print $ is_pretty_match ["8128", "2882", "8281"]
 
