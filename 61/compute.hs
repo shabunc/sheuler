98ea3cbfb5d3800a99c06526c95c843fd71e0ffb
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-18 03:22:21 +0400

    is_pretty_match
    мой код на хаскеле ужасен
    но хорошо я хоть это понимаю пока)

--- 61/compute.hs
+++ 61/compute.hs
@@ -10,15 +10,16 @@ fromto from to ls = dropWhile (< from) $ takeWhile (< to) $ ls
 
 figs n = fromto 1000 10000 $ figures n
 
-are_siblings :: [Int] -> [Int] -> Bool
 are_siblings a b
     | a!!2 == b!!0 && a!!3 == b!!1 = True
     | otherwise = False
 
 digs n = map (digitToInt) $ show n
 
+is_pretty_match l = (length $ filter (\a -> are_siblings (fst a) (snd a)) $ zip l (tail $ cycle l)) == length l
+
 main :: IO()
 main = do
     --print $ [[a,b,c] | a <- figs 3, b <- figs 4, c <- figs 5, are_siblings (digs a) (digs b), are_siblings (digs b) (digs c), are_siblings (digs c) (digs a)]
-    print $  2882 `elemIndex` figures 5
+    print $ is_pretty_match ["8128", "2882", "8281"]
 
