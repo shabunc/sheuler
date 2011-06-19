3ea6e0a20be06cbe1289efa327f647f64f8510e1
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-19 22:49:24 +0400

    всё готово для покорения мира

--- 95/compute.lua
+++ 95/compute.lua
@@ -4,43 +4,49 @@ package.path = package.path .. ";../modules/?.lua"
 require("numeric")
 require("array")
 
+MAX = 10^6
+CHAINLOOKUP = {}
+
 function step(n)
     local divs = numeric.proper_divisors(n)
     local sum = array.reduce(divs, function(a,b) return a + b end)
     return sum
 end
 
-function chain(n, max) 
-    local m = n
-    local len = 0
-    local prev = -1
-    while true do
-        m = step(m)
-        if m == prev then
-            return -1
-        end
-        if m == n then
-            break
-        end
-        if m > max then
-            break
-        end
-        len = len + 1
-        prev = m
+function chain(n, prevt) 
+    if CHAINLOOKUP[n] then
+        return CHAINLOOKUP[n]
     end
-    return len
+    prevt = prevt or {n}
+    local m = step(n)
+    local found = array.index_of(prevt, m)
+    if found == 1 then
+        return #prevt
+    end
+    if found > 1 or m > MAX then
+        return -1
+    end
+    table.insert(prevt, m)
+    local res = chain(m, prevt)
+    CHAINLOOKUP[n] = res
+    return res
 end
 
 function problem95(max) 
     local maxlen, maxn = -1, -1
     for n = 1, max do
-        print(n, chain(n, max))
+        print(n)
+        local len = chain(n)
+        if len > maxlen then
+            maxlen = len
+            maxn = n
+            print(n, len)
+        end
     end
+    print(maxn, maxlen)
 end
 
 assert(step(28) == 28)
 assert(step(220) == 284)
-assert(chain(12496, 10^6) == 4)
 
-
-chain(562, 10^3)
+problem95(MAX)
