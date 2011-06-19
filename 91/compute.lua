379f87e36c68cb2fce58ef782a5c7d3b8806c9eb
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-19 15:00:23 +0400

    для 12 выполняется но явно та же ошибка что и в каселе
    но какая?

--- 91/compute.lua
+++ 91/compute.lua
@@ -17,16 +17,43 @@ function points(n)
     return res
 end
 
-function problem91(n)
-    local res = {}
-    local it = iterator.combinations(2, points(n))
+function are_orthos(ta, tb) 
+    local prod = array.zipWith(function(a, b) return a*b end, ta, tb)
+    local res = array.every(prod, function(a) return a == 0 end)
+    return res
+end
+
+function are_pythas(ta, tb)
+    local td = array.zipWith(function(a, b) return (a - b) end, ta, tb)
+    local it = iterator.combinations(2, {ta, tb, td})
     while true do
         local seq = it()
         if not seq then
             break
         end
-        print(table.concat(seq[1],","), table.concat(seq[2], ","))
+        if are_orthos(seq[1], seq[2]) then
+            return true
+        end
     end
+    return false
 end
 
-problem91(2)
+function problem91(n)
+    local res = {}
+    local it = iterator.combinations(2, points(n))
+    local total = 0
+    while true do
+        local seq = it()
+        if not seq then
+            break
+        end
+        if are_pythas(seq[1],seq[2]) then
+            total = total + 1
+        end
+    end
+    print(string.format("TOTAL %i => %i", n, total))
+    return total
+end
+
+assert(problem91(2) == 12)
+problem91(12)
