0707cde8372de4630605476743aa6502f69d7680
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-20 01:15:37 +0400

    мне предстоит ещё порядочно понять про аргументы в хаскеле

--- 19/compute.hs
+++ 19/compute.hs
@@ -18,14 +18,13 @@ days_in_month year _ = 31
 
 days_in_year year = sum $ map (days_in_month year) [1..12]
 
-day_of_week year month day = ((sum $ map (days_in_year) [1900 .. (year - 1)]) + (sum $ map (days_in_month year) [1 .. (month - 1)]) + (day - 1)) `mod` 7
+day_of_week (year, month, day) = ((sum $ map (days_in_year) [1900 .. (year - 1)]) + (sum $ map (days_in_month year) [1 .. (month - 1)]) + (day - 1)) `mod` 7
 
-nextday :: Int -> Int -> Int -> (Int, Int, Int)
-nextday year 12 31 = (year + 1, 1, 1)
-nextday year month day 
+nextday (year, 12, 31) = (year + 1, 1, 1)
+nextday (year, month, day)
         | day < (days_in_month year month) = (year, month, day + 1)
         | day == (days_in_month year month) = (year, month + 1, 1)
 
 main :: IO()
 main =  do
-    print $ nextday 2011 6 19
+    print $ day_of_week $ nextday (2011, 6, 19)
