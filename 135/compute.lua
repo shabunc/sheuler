ff61ad1bf77dd22944a4c9a99d8040b5732111b2
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-20 00:21:36 +0400

    брат-побратим 136-го
    который, впрочем, мне пока не по зубам

--- 135/compute.lua
+++ 135/compute.lua
@@ -4,40 +4,32 @@ package.path = package.path .. ";../modules/?.lua"
 require("numeric")
 require("array")
 
-function test(n) 
+function solcount(n) 
     local count = 0
     for a = 1, n do
         if n % a == 0 then
             local d = (n/a + a)
             if d % 4 == 0 then
                 if d/4 < a then
-                    if count == 1 then
-                        return false
-                    end
                     count = count + 1
                 end
             end
         end
     end
-    if count == 0 then
-        return false
-    end
-    if count == 1 then
-        return true
-    end
+    return count
 end
 
-function problem136(max)
+function problem135(max)
     local total = 0
     for n = 1, max do
         print(n)
-        if test(n) then
+        if solcount(n) == 10 then
             total = total + 1
         end
     end
     return total
 end
 
-assert(problem136(100) == 25)
+assert(solcount(1155) == 10)
 
-print("TOTAL", problem136(10^6))
+print("TOTAL", problem135(10^6))
