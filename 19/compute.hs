744ce80b6a14c95e057fe22b6e5e59280cc422a2
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-20 00:47:10 +0400

    кажется так правильно

--- 19/compute.hs
+++ 19/compute.hs
@@ -1,12 +1,17 @@
 import Data.List
 
+days_in_month :: Int -> Int -> Int
 --September, April, June and November
 days_in_month year 4 = 30
 days_in_month year 6 = 30
 days_in_month year 9 = 30
 days_in_month year 11 = 30
 
-days_in_month year 2 = 28
+days_in_month year 2 
+    | year `mod` 400 == 0 = 29
+    | year `mod` 100 == 0 = 28
+    | year `mod` 4 == 0 = 29
+    | otherwise = 28
 
 days_in_month year _ = 31
 
@@ -15,4 +20,4 @@ days_in_year year = sum $ map (days_in_month year) [1..12]
 
 main :: IO()
 main =  do
-    print $ days_in_year 1900
+    print $ days_in_year 2000
