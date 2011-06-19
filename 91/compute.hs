0f7aac4211657b33cc3c3f8a35696c9f06e0b9ed
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-19 14:13:24 +0400

    я иду сильно медлененй чем если бы писал на lua
    но это мне поможет в итоге писать быстрее в том числе и на lua

--- 91/compute.hs
+++ 91/compute.hs
@@ -1,8 +1,21 @@
-douce as bs = [(a,b) | a <- as, b <- bs]
+import Data.List
+
+douce as bs = [[a,b] | a <- as, b <- bs]
 points n = tail $ douce [0..n] [0..n]
 
-problem91 n = filter (\xy -> fst(xy) /= snd(xy)) $ douce (points n) (points n)
+combinations :: Int -> [a] -> [[a]]
+combinations 0 _  = [[]]
+combinations n xs = [ y:ys | y:xs' <- tails xs, ys <- combinations (n-1) xs']
+
+pythas :: (Int,Int) -> (Int, Int) -> (Int, Int) -> Bool
+pythas _ _ _ = True
+
+are_orthos :: [Int] -> [Int] -> Bool
+are_orthos xs ys = all (==0) $ zipWith (*) xs ys 
+
+are_pyhtas :: [Int] -> [Int] -> [[Int]]
+are_pyhtas xs ys = [(zipWith (-) xs ys)] ++ [xs] ++ [ys]
 
 main :: IO()
 main = do
-    print $ problem91 2
+    print $ are_pyhtas [1,0] [1,2]
