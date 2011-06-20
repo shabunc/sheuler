69004d384f11c7c21ffbbdcc42f20c9a302aeb5a
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-20 22:56:13 +0400

    fraction reduce

--- modules/fraction.lua
+++ modules/fraction.lua
@@ -1,6 +1,6 @@
 require("array")
 require("class")
-require("nuneric")
+require("numeric")
 
 fraction = class:new()
 
@@ -11,4 +11,18 @@ end
 
 function fraction:__tostring()
     return "" .. self.a .. "/" .. self.b
+end
+
+function fraction:reduce() 
+    local a, b = self.a, self.b
+    local ra, rb = a, b
+    local n = math.min(a, b)
+    for j = 1, n do
+        if a % j == 0 and b % j == 0 then
+            ra = a / j
+            rb = b / j
+        end
+    end
+    self.a, self.b = ra, rb
+    return self
 end
