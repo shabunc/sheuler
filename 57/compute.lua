e228e712dfc03b4abc669598de063a0c7c8066bb
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-21 07:35:45 +0400

    напрямую вывожу

---
+++ 57/compute.lua
@@ -0,0 +1,43 @@
+#!/usr/bin/env lua
+package.path = package.path .. ";../modules/?.lua"
+
+require("numeric")
+require("array")
+require("fraction")
+
+local ONE = fraction(1, 1)
+
+function step(fr)
+    return fraction(fr.a + 2 * fr.b, fr.a + fr.b):reduce()
+end
+
+function numlen(n)
+    return math.floor(math.log10(n)) + 1
+end
+
+function good_one(fr) 
+    if fr.b >= fr.a then
+        return false
+    end
+    if numlen(fr.b) >= numlen(fr.a) then
+        return false
+    end
+    return true
+end
+
+function problem57(n)
+    local fr = fraction(1, 1)
+    local total = 0
+    for j = 1, n do 
+        fr = step(fr)
+        local passed = good_one(fr)
+        print(j, fr, passed)
+        if passed then
+            total = total + 1
+        end
+    end
+    print("TOTAL", total)
+    return total
+end
+
+problem57(1000)
--- modules/fraction.lua
+++ modules/fraction.lua
@@ -17,7 +17,7 @@ function fraction:reduce()
     local a, b = self.a, self.b
     local ra, rb = a, b
     local n = math.min(a, b)
-    for j = 1, n do
+    for j = 1, math.sqrt(n) do
         if a % j == 0 and b % j == 0 then
             ra = a / j
             rb = b / j
@@ -25,4 +25,16 @@ function fraction:reduce()
     end
     self.a, self.b = ra, rb
     return self
+end
+
+function fraction.add(f1, f2) 
+    return fraction(f1.a * f2.b + f1.b * f2.a, f1.b * f2.b):reduce()
+end
+
+function fraction.mul(f1, f2)
+    return fraction(f1.a * f2.a, f1.b * f2.b):reduce()
+end
+
+function fraction:recip()
+    return fraction(self.b, self.a):reduce()
 end
