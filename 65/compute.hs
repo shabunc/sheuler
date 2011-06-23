95728c59ef1208e5e42b55b920b1a50682a462a8
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-23 09:39:48 +0400

    как-то так

--- 65/compute.hs
+++ 65/compute.hs
@@ -4,8 +4,8 @@ reduce (a, b) = (a `div` (gcd a b), b `div` (gcd a b))
 
 step (a, b) n g  = reduce $ (a + ((g n) + 1) * b, a + (g n) *b)
 
-sq (a, b) n = step (a, b) n (\a -> 1)
+sq ((a, b), n) = (step (a, b) n (\a -> 1), n + 1)
 
 main :: IO()
 main = do
-    print $ sq (1, 1) 1
+    print $ take 5 $ iterate (sq) ((1, 1), 1)
