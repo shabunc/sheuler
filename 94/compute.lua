330c24dcf708d968dd73218b584612d131e18de7
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-29 09:07:42 +0400

    и снова мимо кассы (((

--- 94/compute.lua
+++ 94/compute.lua
@@ -10,21 +10,23 @@ end
 
 TOTAL = 0
 
-function findall(a)
+function findall(p)
     for j = -1, 1, 2 do
-        local b = 2*a - j
-        local h = b^2 - a^2 
-        if is_square(h) then
-            print(2*a, b, math.sqrt(h), 2*a + 2*b)
-            TOTAL = TOTAL + (2*a + 2*b)
+        if (p + j) % 3 == 0 then
+            local a = (p + j) / 3
+            local b = a - j
+            local h  = math.sqrt(b^2 - (0.5*a)^2)
+            if h > 0 and math.floor(h) == math.ceil(h) then
+                print(a, b, h, a + 2*b)
+                TOTAL = TOTAL + (a + 2*b)
+            end
         end
     end
 end
 
 function problem94(n) 
-    local max = (n + 1)/6
-    for a = 0.5, max, 0.5 do
-        findall(a)
+    for p = 2, n do 
+        findall(p)
     end
     print("TOTAL", TOTAL)
 end
