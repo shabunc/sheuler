8147667921c71488e64f860796c3423b4e1ae1e5
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-29 10:18:13 +0400

    нулевой подход к снаряду

--- 59/compute.lua
+++ 59/compute.lua
@@ -22,6 +22,24 @@ function freqs(data)
     return freqs
 end
 
+function xor(a, b) 
+    return (a + b) - (a and b) * 2
+end
+
+function decipher(key, message)
+    local deciphered = array{}
+    for i, v in ipairs(message) do
+       local ch = string.byte(key[i % #key + 1])
+       local dch = xor(ch, v)
+       table.insert(deciphered, string.char(dch))
+    end
+    return deciphered
+end
+
+--[[
 for _, v in ipairs(freqs(DATA)) do
     print(v[1], v[2])
 end
+]]
+
+print(decipher({"a", "b", "c"}, DATA))
