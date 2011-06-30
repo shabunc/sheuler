9ca884c44fd5c56beea6d78b5d7ea4eb97c35acd
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-30 23:34:30 +0400

    долго думал отчего не сходится
    а там переполнение)

--- 18/compute.lua
+++ 18/compute.lua
@@ -4,11 +4,30 @@ package.path = package.path .. ";../modules/?.lua"
 require("numeric")
 require("array")
 
+function add_left(tree, child)
+    tree.left = child
+end
+
+function add_right(tree, child)
+    tree.right = child
+end
+
 function tree(val, left, right) 
     local root = {}
     root.val = val
-    root.left = left
-    root.right = right
-    return root
+    add_left(root, left)
+    add_right(root, right)
 end
 
+
+function layer(nodes, vals)
+    local subnodes = {}
+    for _, v in ipairs(vals) do
+        table.insert(subnodes, vals[v])
+    end
+    add_left(nodes[1], vals[1])
+    add_right(nodes[1], vals[2])
+end
+
+local ROOT = {}
+layer(ROOT, 
