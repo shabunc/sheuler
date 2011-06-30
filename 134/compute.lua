463fcf0d1a844e658f35744a362bbe31935280bf
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-06-30 11:29:28 +0400

    всё готово к бою

--- 134/compute.lua
+++ 134/compute.lua
@@ -7,12 +7,30 @@ require("array")
 function smallest(n)
     local p1 = numeric.prime(n)
     local p2 = numeric.prime(n + 1)
+    local numlen = math.floor(math.log10(p1)) + 1
     for j = 1, math.huge do
-        local m = j * 100 + p1
+        local m = j * 10 ^ numlen + p1
         if m % p2 == 0 then
             return m
         end
     end
 end
 
-smallest(8)
+function problem134(max)
+    local total = 0
+    for j = 3,  math.huge do
+        local p = numeric.prime(j)
+        if p > max then
+            break
+        end
+        local sm = smallest(j)
+        print(sm, p)
+        total = total + sm
+    end
+    print("TOTAL", total)
+    return total
+end
+
+assert(smallest(8) == 1219)
+
+problem134(10^3)
