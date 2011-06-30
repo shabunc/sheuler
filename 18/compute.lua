eb2d1b990e7982aedf7517f6ab5a2a754f4c1e25
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-01 00:23:11 +0400

    так - фигня какая-то

--- 18/compute.lua
+++ 18/compute.lua
@@ -4,30 +4,43 @@ package.path = package.path .. ";../modules/?.lua"
 require("numeric")
 require("array")
 
-function add_left(tree, child)
-    tree.left = child
+function add_left(root, lval)
+    root.left = tree(lval)
 end
 
-function add_right(tree, child)
-    tree.right = child
+function add_right(root, rval)
+    root.right = tree(rval)
 end
 
-function tree(val, left, right) 
+function tree(val)
     local root = {}
     root.val = val
-    add_left(root, left)
-    add_right(root, right)
+    return root
 end
 
 
 function layer(nodes, vals)
-    local subnodes = {}
-    for _, v in ipairs(vals) do
-        table.insert(subnodes, vals[v])
-    end
     add_left(nodes[1], vals[1])
-    add_right(nodes[1], vals[2])
+    local subnodes = {nodes[1].left}
+    for j = 1, #nodes do
+        add_right(nodes[j], vals[j + 1])
+        table.insert(subnodes, nodes[j].right)
+    end
+    return subnodes
 end
 
-local ROOT = {}
-layer(ROOT, 
+function maketree(vals) 
+    local root = tree(vals[1])
+    local subs = {root}
+    local count = 2
+    while count < (#vals + 1) do
+        add_left(subs[1], vals[count])
+        for j = 1, #subs do
+            add_right(subs[j], vals[count] + 1
+        end
+    end
+    return root
+end
+
+local ROOT = maketree({3, 7, 4})
+print(ROOT.left)
