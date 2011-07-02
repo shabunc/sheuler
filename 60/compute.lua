9ed3d11da91f9a082c285036a8f75e14da7a4554
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-03 03:06:06 +0400

    начал перелопачивать

--- 60/compute.lua
+++ 60/compute.lua
@@ -13,3 +13,46 @@ end
 assert(remarkable_pair(3, 109))
 assert(not remarkable_pair(2, 109))
 
+
+function remarkable_to(p, tail) 
+    for j = 1, #tail do
+        if not remarkable_pair(p, tail[j]) then
+            return false
+        end
+    end
+    return true
+end
+
+function remarkable_generator(n, rems)
+    if n == 0 then
+        coroutine.yield(rems)
+    else
+        for j = 1, math.huge do
+            local p = numeric.prime(j)
+            if remarkable_to(p, rems) then
+                local tail = array(rems)
+                table.insert(tail, 1, p)
+                remarkable_generator(n - 1, tail)
+            end
+        end
+    end
+end
+
+function remarkable_iterator(n)
+    return coroutine.wrap(function() 
+        return remarkable_generator(n, {})
+    end)
+end
+
+function problem60(n)
+    local it = remarkable_iterator(n)
+    while true do
+        local seq = it()
+        if not seq then
+            break 
+        end 
+        print(table.concat(seq))
+    end
+end
+
+problem60(2)
