e88d95a30759ba2d05bcf0710c24145a3f6cf44a
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-03 01:51:17 +0400

    проверка на вхождение
    вдруг понял что одно и то же число может попадаться
    несколько раз

--- 87/compute.lua
+++ 87/compute.lua
@@ -21,8 +21,10 @@ function generator(max, ...)
 end
 
 function find_all(max)
+    local found = {}
     local total = 0
     for j = 1, math.huge do
+        local jp = numeric.prime(j)
         for i = j, 1, -1 do
             for k = i, 1, -1 do
                 local jp, ip, kp = numeric.prime(j), numeric.prime(i), numeric.prime(k)
@@ -38,8 +40,11 @@ function find_all(max)
                     local a, b, c = unpack(seq)
                     local val = a^2 + b^3 + c^4
                     if val < max then
-                        total = total + 1
-                        print(a, b, c, "=>", val, " => ", total)
+                        if not found[val] then
+                            found[val] = true
+                            total = total + 1
+                            print(a, b, c, "=>", val, " => ", total)
+                        end
                     end
                 end
             end
