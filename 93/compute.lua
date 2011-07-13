2dcc4981d6a102e401a31d75b4912fa71faf53df
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-14 00:22:52 +0400

    expression
    вообще не знаю с чего начать в этой задаче.
    значит,  с чего-то нужно начать)

---
+++ 93/compute.lua
@@ -0,0 +1,43 @@
+#!/usr/bin/env lua
+package.path = package.path .. ";../modules/?.lua"
+
+require("numeric")
+require("array")
+
+--[[
+    MINUS NUMBER | EXPRESSION
+    PLUS  (NUMBER | EXPRESSION) *
+    MUL   (NUMBER | EXPRESSION)
+    DIV   (NUMBER | EXPRESSION) (NUBER | EXPRESSION)
+]]
+
+function express(op, ...)
+    if op == '-' then
+        local a = select(1, ...)
+        return -a
+    end
+    if op == '/' then
+        local a = select(1, ...)
+        local b = select(2, ...)
+        return a / b
+    end
+    if op == '+' then
+        local sum = 0
+        for i, v in ipairs{...} do
+            sum = sum + v
+        end
+        return sum
+    end
+    if op == '*' then
+        local prod = 1
+        for i, v in ipairs{...} do
+            prod = prod * v
+        end
+        return prod
+    end
+end
+
+assert(express("-", 5) == -5)
+assert(express("+", 1, 2, 3) == 6)
+assert(express("*", 1, 2, 3, 4) == 24)
+assert(express("/", 20, 5) == 4)
