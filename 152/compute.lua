90e2b8039dc2203f793105646771e8d4becbc941
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-16 00:04:20 +0400

    в теории это уже должно дать решение
    но ждать этого регения конечно прийдётся долго)

--- 152/compute.lua
+++ 152/compute.lua
@@ -23,6 +23,29 @@ function iterator(n, t)
     end)
 end
 
+function squares(t, skip_index) 
+    local res = bignum{1}
+    for j = 1, #t do 
+        if j ~= skip_index then
+            local n = bignum(numeric.digits(t[j]))  
+            res = res:mul(n:mul(n))
+        end
+    end
+    return res
+end
+
+function sumall(t)
+    local sum = bignum{0}
+    for skip_index = 1, #t do
+        sum = sum:add(squares(t, skip_index))
+    end
+    local total = squares(t, #t + 1)
+    return sum:mul(bignum{2}) == total
+end
+
+assert(sumall{2,3,4,5,7,12,15,20,28,35} == true)
+assert(sumall{2,3,4,6,7,9,10,20,28,35,36,45} == true)
+
 function problem152(t)
     for n = 2, #t do
         local it = iterator(n, t)
@@ -31,9 +54,12 @@ function problem152(t)
             if not seq then
                 break
             end
-            print(table.concat(seq," "))
+            if sumall(seq) then
+                print(table.concat(seq," "))
+            end
         end
     end
 end
 
-problem152({2,3,4,5,7,12,15,20,28,35})
+
+problem152(array.range(2, 45))
