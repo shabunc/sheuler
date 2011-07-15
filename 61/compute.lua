3415a140cc8cc0d2cd000e83294b877fba7c8623
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-15 09:20:20 +0400

    подготовка к бою
    пусть некарсиво, но зато наводит на мысли

---
+++ 61/compute.lua
@@ -0,0 +1,76 @@
+#!/usr/bin/env lua
+package.path = package.path .. ";../modules/?.lua"
+
+require("numeric")
+require("array")
+
+
+function tri(n) 
+    return n * (n + 1) * 0.5
+end
+
+function square(n)
+    return n * n
+end
+
+function penta(n)
+    return n * (3 * n - 1) * 0.5
+end
+
+function hexa(n)
+    return n * (2 * n - 1)
+end
+
+function hepta(n)
+    return n * (5 * n - 3) * 0.5
+end
+
+function octa(n)
+    return n * (3 * n - 2)
+end
+
+function collect(func, from, to)
+    local res = {}
+    for j = 1, math.huge do
+        if func(j) > to then
+            break
+        end
+        if func(j) >= from then
+            res[func(j)] = true
+        end
+    end
+    return res
+end
+
+function contains_in(el, list)
+    for _, v in ipairs(list) do
+        if v[el] then
+            return true, _
+        end
+    end
+    return false
+end
+
+function problem61()
+    local tris = collect(tri, 10^3, 10^4)
+    local sqs = collect(square, 10^3, 10^4)
+    local pentas = collect(penta, 10^3, 10^4)
+    local figs = {tris, sqs, pentas}
+    for t = 10, 99 do
+        for s = 10, 99 do
+            for p = 10, 99 do
+                local n1 = 100 * t + s
+                local c1, i1 = contains_in(n1, figs)
+                local n2 = 100 * s + p
+                local c2, i2 = contains_in(n2, figs)
+                local n3 = 100 * p + t
+                local c3, i3 = contains_in(n3, figs)
+                if c1 and c2 and c3 then
+                    print(n1, n2, n3, i1, i2, i3)
+                end
+            end
+        end
+    end
+end
+
+problem61()
