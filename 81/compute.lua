3ff143b1ad4f386816ce24801711d2ede7c6f6ec
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-17 00:45:56 +0400

    где-то близко

--- 81/compute.lua
+++ 81/compute.lua
@@ -4,7 +4,7 @@ package.path = package.path .. ";../modules/?.lua"
 require("numeric")
 require("array")
 
-data = {
+RECT = {
     {131, 673, 234, 103, 18},
     {201, 96,  342, 965, 150},
     {630, 803, 746, 422, 111},
@@ -12,35 +12,35 @@ data = {
     {805, 732, 524, 37,  331}
 }
 
-function step_gen(data)
-    local step = function(x, y) 
-       local nx, ny
-       if x == #data[1] then
-            nx, ny = x, y + 1
-       end
-       if y == #data[1] then
-            nx, ny = x  + 1, y
-       end
-       if data[x + 1][y] < data[x][y + 1] then
-            nx, ny = x + 1, y
-       else
-            nx, ny = x, y + 1     
-       end
-       return data[nx][ny], nx, ny
+LILRECT = {
+    {746, 422, 111},
+    {497, 121, 956},
+    {524, 37,  331}
+}
+
+TINYRECT = {
+    {121, 956},
+    {37,  331}
+}
+
+function borrow(b, a)
+    return setmetatable(b, {
+        __index = function(self, key)
+            return rawget(self, key) or a[key]
+        end
+    })
+end  
+
+function sumrect(rect)
+    local r = borrow({}, rect)
+    for row = #rect - 1, 1, -1 do
+        for j = #rect[row] - 1, 1, -1 do
+            r[row][j] = rect[row][j] + math.min(rect[row + 1][j], rect[row][j + 1]) + r[row + 1][j + 1]
+            print(row, j, r[row][j])
+        end
     end
-
-    return step
-end 
-
-
-function problem81(data)
-    local step = step_gen(data)
-    local val, x, y = step(1, 1)
-    print(val, x, y)
-    repeat
-        val, x, y = step(x, y)
-        print(val, x, y)
-    until not val
+    return r
 end
 
-problem81(data)
+print(sumrect(TINYRECT)[1][1])
+print(sumrect(LILRECT)[1][1])
