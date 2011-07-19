d18b7c678fc94c18601c9f6b5922d661176b0cf1
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-19 08:58:00 +0400

    где-то близко

--- 225/compute.lua
+++ 225/compute.lua
@@ -15,10 +15,34 @@ tris = memoize(tris)
 
 function collect(max)
     local n = 1
+    local res = {}
     while tris(n) < max do
+        local divs = numeric.propers(tris(n))
+        for _, d in ipairs(divs) do
+            table.insert(res, d)
+        end
         n = n + 1
+    end
+    table.sort(res)
+    res = array.uniq(res)
+    return res
+end
+
+function problem225(n)
+    local divs = collect(2 * 10^17)
+    local count = 0
+    print("found ", #divs)
+    for j = 1, divs[#divs], 2 do
+        found = array.index_of(divs, j)
+        if found == 0 then
+            print(count, j, found)
+            count = count + 1
+            if count  == n  then
+                break
+            end
+        end
     end
 end
 
+problem225(124)
 
-collect(10^9)
