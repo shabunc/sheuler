f144e4a2b4b334ba4b910bab3701d16ee60d60cc
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-26 01:58:05 +0400

    вычисление н-й дроби
    теперь нужно лишь поменять на bignum

--- 65/compute.hs
+++ 65/compute.hs
@@ -2,19 +2,13 @@ import Data.List
 
 reduce (a, b) = (a `div` (gcd a b), b `div` (gcd a b))
 
-invert (a, b) = (b, a)
-add (a, b) (c, d) = reduce (a * d + c * b, b * d)
-
-step (c, d) (a, b) = add (2, 1) $ invert $ add (c, d) (a, b) 
-
-sq ((a, b), n) = (step (1,1) (a, b), n + 1)
-
 fexp n
     | (n - 1) `mod` 3 == 0 = (0, 1)
     | (n - 1) `mod` 3 == 1 = (2 * ((n - 2) `div` 3 + 1) - 1, 1)
     | otherwise = (0, 1)
 
-rexp ((a, b), n) = (step (fexp n) (a, b), n + 1) 
+h -2 a = (0, 1)
+h -1 a = (1, 0)
 
 main :: IO()
 main = do
