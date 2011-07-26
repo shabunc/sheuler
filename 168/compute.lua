d3d0c0002169e69b7d9d33dbc24d0b659e0f38fc
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-26 22:22:08 +0400

    теперь осталось самое лёгкое
    решить задачу))))

--- 168/compute.lua
+++ 168/compute.lua
@@ -4,8 +4,8 @@ package.path = package.path .. ";../modules/?.lua"
 require("numeric")
 require("array")
 
-function step(n, rem, len)
-    return coroutine.wrap(function() 
+function step(len)
+    local function step_generator(n, rem, len)
         for m = 1, 9 do
             local pr = n * m + rem
             local r = pr % 10
@@ -13,10 +13,15 @@ function step(n, rem, len)
                 coroutine.yield(m, math.floor(pr / 10))
             end
         end
+    end
+    return coroutine.wrap(function() 
+        for n = 1, 9 do
+            step_generator(n, 0, len)
+        end
     end)
 end
 
-local it  = step(1, 0)
+local it  = step(3)
 while true do
     local seq, rem = it()
     if not seq then
