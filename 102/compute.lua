cb7d37c1e95b25921782dfd30be061b1546f541a
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-31 02:53:34 +0400

    ура!
    а то закопался я с этой проклятой 108-й
    надо отвлекаться иногда
    хотя бы для поднятия самооценки)

--- 102/compute.lua
+++ 102/compute.lua
@@ -25,11 +25,27 @@ function good(a, b, c)
     local ac = math.acos(cos(a, c))
     local bc = math.acos(cos(b, c))
     local sum = ab + ac + bc
-    if (sum == 2 * math.pi) then
+    if (math.abs(sum - 2 * math.pi) < 0.000001) then
         return true
     end
     return false
 end
 
+function problem102(data)
+    local count = 0
+    for _, tr in ipairs(data) do
+       local a = {tr[1], tr[2]} 
+       local b = {tr[3], tr[4]} 
+       local c = {tr[5], tr[6]} 
+       local test = good(a, b, c)
+       if test then
+            count = count + 1
+       end
+    end
+    return count
+end
+
 assert(good({-340, 495},{-153, -910}, {835, -947}) == true)
 assert(good({-175, 41},{-421, -714}, {574, -645}) == false)
+
+print(problem102(TRIANGLES))
