1ad7aab1a7daa603320ca4f63a74cb58c62cb1ff
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-07-31 12:39:26 +0400

    подготовка к бою

---
+++ 138/compute.lua
@@ -0,0 +1,52 @@
+#!/usr/bin/env lua
+package.path = package.path .. ";../modules/?.lua"
+
+require("numeric")
+require("array")
+
+function step(a, b, c, maxp)
+    local function gen(a, b, c, maxp)
+        coroutine.yield(a, b, c, a + b + c)
+        for j = -1, 1, 2 do
+            for i = -1, 1, 2 do
+                local a = a * j
+                local b = b *  i
+                local na = a + 2 * b + 2 *c
+                local nb = 2 * a + b + 2 * c
+                local nc = 2 * a + 2 * b + 3 * c
+                local p = na + nb + nc
+                if na > 0 and nb > 0 and nc > 0 and p <= maxp then
+                    gen(na, nb, nc, maxp)
+                end
+            end
+        end
+    end
+    return coroutine.wrap(function() 
+        return gen(a, b, c, maxp)
+    end)
+end
+
+function genall(maxp) 
+    local a, b, c = 3, 4, 5
+    local it = step(a, b, c, maxp)
+    local total = 0
+    local sum = 0
+    while true do 
+        local  a, b, c, p = it()
+        local  t = {a, b, c}
+        table.sort(t)
+        a, b, c = unpack(t)
+        if not a then
+            break
+        end
+        if (math.abs(2 * a -  b) == 1) then
+            print(a, b, c)
+            total = total + 1
+            sum = sum + c
+        end
+    end
+    print("TOTAL ", total)
+end
+
+genall(10^9)
+
