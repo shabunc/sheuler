fa5fcc0e2e2e0330731025e3925fca1eb17a3afd
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-08-16 00:54:58 +0400

    73702
    100!!!!

--- 105/compute.lua
+++ 105/compute.lua
@@ -3,6 +3,8 @@ package.path = package.path .. ";../modules/?.lua"
 
 require("array")
 
+dofile("sets.txt")
+
 function biterator(n)
     local function gen(n, t)
         if n == 0 then
@@ -67,5 +69,19 @@ function is_good(t)
     return true
 end
 
+function problem105(data)
+    local total = 0
+    for _, t in ipairs(data) do
+        print(_)
+        if is_good(t) then
+            total = total + sum(t)
+        end
+    end
+    print("TOTAL", total)
+    return total
+end
+
 assert(is_good({81, 88, 75, 42, 87, 84, 86, 65}) == false)
 assert(is_good({157, 150, 164, 119, 79, 159, 161, 139, 158}) == true)
+
+problem105(DATA)
