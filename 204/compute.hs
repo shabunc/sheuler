8f46131f8107cbdfd8bc57870540b9105939cf03
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-09-01 11:27:58 +0400

    2944730

--- 204/compute.hs
+++ 204/compute.hs
@@ -7,35 +7,42 @@ merge xs@(x : xs') ys@(y : ys')
     | x < y  = x : merge xs' ys 
     | otherwise = y : merge xs ys'
 
+ham n = map (n *)
+
+hamming :: [Integer]
+hamming = 1 : foldl1 (merge) [
+                    ham 2 hamming, 
+                    ham 3 hamming,
+                    ham 5 hamming,
+                    ham 7 hamming,
+                    ham 11 hamming,
+                    ham 13 hamming,
+                    ham 17 hamming,
+                    ham 19 hamming,
+                    ham 23 hamming,
+                    ham 29 hamming,
+                    ham 31 hamming,
+                    ham 37 hamming,
+                    ham 41 hamming,
+                    ham 43 hamming,
+                    ham 47 hamming,
+                    ham 53 hamming,
+                    ham 59 hamming,
+                    ham 61 hamming,
+                    ham 67 hamming,
+                    ham 71 hamming,
+                    ham 73 hamming,
+                    ham 79 hamming,
+                    ham 83 hamming,
+                    ham 89 hamming,
+                    ham 97 hamming
+              ]
+
 
 main :: IO()
 main = do
-    print $ length $ takeWhile (< 1000000000) $ foldl1 (merge) [
-                                      (map (2 *) [1..]), 
-                                      (map (3 *) [1..]),
-                                      (map (5 *) [1..]),
-                                      (map (7 *) [1..]),
-                                      (map (11 *) [1..]),
-                                      (map (13 *) [1..]),
-                                      (map (17 *) [1..]),
-                                      (map (19 *) [1..]),
-                                      (map (23 *) [1..]),
-                                      (map (29 *) [1..]),
-                                      (map (31 *) [1..]),
-                                      (map (37 *) [1..]),
-                                      (map (41 *) [1..]),
-                                      (map (43 *) [1..]),
-                                      (map (53 *) [1..]),
-                                      (map (59 *) [1..]),
-                                      (map (61 *) [1..]),
-                                      (map (67 *) [1..]),
-                                      (map (71 *) [1..]),
-                                      (map (73 *) [1..]),
-                                      (map (79 *) [1..]),
-                                      (map (83 *) [1..]),
-                                      (map (89 *) [1..]),
-                                      (map (97 *) [1..])
-                                    ]
+    print $ length $ takeWhile (<= 1000000000) hamming
+
 
 
 
--- 204/compute.lua
+++ 204/compute.lua
@@ -3,33 +3,52 @@ package.path = package.path .. ";../modules/?.lua"
 
 require("array")
 
-function merge(ta, tb)
-    local a = 1
-    local b = 1
+function lazy(f, cur)
     local res = {}
-    while a < #ta and b < #tb do
-        if ta[a] == tb[b] then
-            table.insert(res, ta[a]) 
-            a = a + 1
-            b = b + 1
-        elseif ta[a] < tb[b] then
-            table.insert(res, ta[a])
-            a = a + 1
-        else
-            table.insert(res, tb[b])
-            b = b + 1
-        end
+    local current 
+    local step = 1
+    res.inspect = function() 
+        return current
     end
-    if a < #ta then
-        for j = a, #ta do
-            table.insert(res, ta[j])
-        end
-    else
-        for j = b, #tb do
-            table.insert(res, tb[j])
-        end
+    res.move = function() 
+        current = f(step)
+        step  = step + 1
+        return res
     end
-    return res
+    return res.move()
 end
 
-print(table.concat(merge({1, 7, 9}, {2, 3, 9, 11}), " "))
+function ham(n, lt)
+    return lazy(function() 
+        return n * lt.inspect()
+    end)
+end
+
+
+function merge(la, lb)
+    local function movefunc()
+        local a = la.inspect()
+        local b = lb.inspect()
+        local res
+        if a == b then
+            res = a
+            la.move()
+            lb.move()
+        elseif a < b then
+            res = a
+            la.move()
+        else
+            res = b
+            lb.move()
+        end
+        return res
+    end
+    return lazy(movefunc)
+end
+
+
+local h = hamming()
+for j = 1, 10 do
+    print(h.inspect())
+    h.move()
+end
