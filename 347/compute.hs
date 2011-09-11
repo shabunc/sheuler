9bef8038f6abfc2baf14af869a8a75133b12b2e9
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-09-11 17:31:15 +0400

    somewhere near

--- 347/compute.hs
+++ 347/compute.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Control.Monad
 
 merge [] ys = ys
 merge xs [] = xs
@@ -13,8 +14,16 @@ hamming p q = p*q : merge (map (p *) ham) (map (q *) ham)
 
 m p q n 
     | (p * q) > n = 0
+    | p >= q = 0
     | otherwise = last $ takeWhile (< n) $ hamming p q
+
+is_prime :: Integer -> Bool
+is_prime n = null (filter ((==0). mod n) [2..floor(fromIntegral n ** 0.5)]) 
+
+primes n = filter is_prime [2 .. n]
 
 main :: IO()
 main = do
-    print $ m 3 55 100
+    print $ sum $ map (\a -> m (fst a) (snd a) 100) $ liftM2(,) prim prim
+                where prim = primes 100
+
