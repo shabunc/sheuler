c24c0b32ffe944f7c39bb44de246e24e62130bf8
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-09-11 17:47:24 +0400

    combs

--- 347/compute.hs
+++ 347/compute.hs
@@ -13,17 +13,21 @@ hamming p q = p*q : merge (map (p *) ham) (map (q *) ham)
                 where ham = hamming p q
 
 m p q n 
-    | (p * q) > n = 0
     | p >= q = 0
+    | (p * q) > n = 0
     | otherwise = last $ takeWhile (< n) $ hamming p q
 
 is_prime :: Integer -> Bool
 is_prime n = null (filter ((==0). mod n) [2..floor(fromIntegral n ** 0.5)]) 
 
+combs 0 _ = [[]]
+combs k [] = []
+combs k (x:xs) = map (x:) (combs (k - 1) xs) ++ combs k xs
+
 primes n = filter is_prime [2 .. n]
 
 main :: IO()
 main = do
-    print $ sum $ map (\a -> m (fst a) (snd a) 100) $ liftM2(,) prim prim
+    print $ sum $ map (\a -> m (a!!0) (a!!1) 100) $ combs 2 prim
                 where prim = primes 100
 
