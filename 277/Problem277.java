3535430cf7d5a4ab4aab76841f0cc449e6589cc0
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-10-18 02:19:53 +0400

    подготовка к бою

---
+++ 277/Problem277.java
@@ -0,0 +1,46 @@
+
+class Problem277 {
+
+    enum Step {
+        U, D, d
+    }
+
+    private Step step;
+    private long val;
+
+    public Problem277(long val) {
+        this.val = val;
+    }
+
+    public void nextStep() {
+        long rem = val % 3;
+        if (rem == 0) {
+            val = val / 3;
+            step = step.D;
+        } else
+        if (rem == 1) {
+            val = (4 * val + 2) / 3;
+            step = step.U;
+        } else {
+            val = (2 * val - 1) / 3;
+            step = step.d;
+        }
+        System.out.print(val);
+        System.out.println(step);
+    }
+
+    public String getSequence(int len) {
+        String s = "";
+        for (int i = 0; i < len; i++) {
+           nextStep();
+           s += step;
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Problem277 st = new Problem277(104064);
+        System.out.println(st.getSequence(10));
+    }
+
+}
