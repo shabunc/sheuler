7f65fc338f918ac015e8773233bdf487263cadf6
Shagen Oganjanyan <oganjanyan@corp.mail.ru>
2011-11-01 10:46:38 +0400

    переписал c hs на lua
    теперь нужно добавить memoize

--- 345/compute.lua
+++ 345/compute.lua
@@ -7,163 +7,66 @@ require("array")
 
 dofile("data.lua")
 
-function nextperm(a) 
+tiny = {{7, 53}, {497, 383}}
+
+function copy(t)
     local res = {}
-    --copy
-    for j = 1, #a do
-        table.insert(res, a[j])
-    end
-    local k = #a - 1
-    --find such k that a[k] < a[k + 1] > a[k] > ... > a[n]
-    while a[k] > a[k + 1] do
-        k = k - 1
-        if k == 0 then
-            return false
+    for j = 1, #t do 
+        local val = t[j]
+        if (type(val) == "table") then
+            table.insert(res, copy(val))
+        else 
+            table.insert(res, val)
         end
-    end
-    local m = #a 
-    --find such m that it it the least a[m] such that a[m] > a[k]
-    while a[m] < a[k] do
-        m = m - 1
-    end
-    res[k], res[m] = res[m], res[k]
-    --revert
-    local swap = {}
-    for j = k + 1, #a do
-        table.insert(swap, table.remove(res)) 
-    end
-    for j = 1, #swap do
-        table.insert(res, table.remove(swap))
     end
     return res
 end
 
-function apply_perm(matrix, perm) 
+function remrow(t, j)
+    local ct = copy(t)
+    table.remove(ct, j)
+    return ct
+end
+
+function remcol(t, j)
+    local ct = copy(t)
+    for i = 1, #ct do
+        table.remove(ct[i], j)
+    end
+    return ct
+end
+
+function remove(t, i, j)
+    return remrow(remcol(t, j), i)
+end
+
+function ranks(m)
     local res = {}
-    for j = 1, #perm do
-        table.insert(res, matrix[j][perm[j]]) 
+    local n = #m
+    for i = 1, n do
+        for j = 1, n do
+            table.insert(res, {remove(m, i, j), m[i][j]})
+        end
     end
     return res
 end
 
-function problem345(matrix) 
-    --brute
-    local range = {}
-    local max = -1
-    for j = 1, #matrix do
-       table.insert(range, j) 
+function msum(t) 
+    if #t == 1 then
+        return t[1][1]
     end
-    local it = iterator.perm_lex(range)
-    while true do
-       local seq =  it()
-       if not seq then
-          break
-       end
-       local cseq = apply_perm(matrix, seq)
-       local sum = array.reduce(cseq, function(a, b) return a + b end)
-       if sum > max then
-            print(table.concat(seq," "), sum)
-            max = sum
-        end
-    end
-    return max
-end
-
-function tupstring(self)
-    local str = ""
-    for j = 1, #self do
-        str = str .. "(" .. self[j][1] .. "," .. self[j][2] ..")"
-    end
-    return str
-end
-
-function prepare(t)
+    local rnk = ranks(t)
     local res = {}
-    for j = 1, #t do
-        table.insert(res, {t[j], j})
+    for j = 1, #rnk do
+        table.insert(res, msum(rnk[j][1]) + rnk[j][2])
     end
-    table.sort(res, function(a, b) 
-        return a[1] > b[1]
-    end)
-    return setmetatable(res,{
-        __tostring = tupstring
-    });
-end
-
-function prepare_all(m) 
-    local res = array(m):map(prepare)
-    return setmetatable(res, {
-        __tostring = function(self) 
-            local str = ""
-            for j = 1, #self do
-                str = str .. tostring(self[j]) .. "\n" 
-            end
-            return str
-        end
-    })
-end
-
-function inc(t, maxdig)
-    local k = #t
-    while t[k]  == maxdig do
-        t[k] = 1
-        k = k - 1
-    end
-    if k == 0 then
-        return false
-    end
-    t[k] = t[k] + 1
-    return t
-end
-
-function slice(m, t)
-    local res = {}
-    for j = 1, #t do
-        table.insert(res, m[j][t[j]])
-    end
-    return setmetatable(res, {
-        __tostring = tupstring
-    })
-end
-
-function is_good(t)
-    local uniq = {}
-    for j = 1, #t do
-        if uniq[t[j][2]] then
-            return false
-        end
-        uniq[t[j][2]] = 1
-    end     
-    return true
-end
-
-function ids(len)
-    local res = {}
-    for j = 1, len do
-        table.insert(res, 1)
-    end
-    return res
-end
-
-function solve(m)
-    local pm = prepare_all(m)
-    local it = ids(#m)
-    local max = -1
-    while true do
-        --print(table.concat(it, " "))
-        local sm = slice(pm, it)
-        if is_good(sm) then
-            local sum = array(sm):reduce(function(a, b) return a + b[1] end)
-            print(sum, max)
-            if sum > max then
-                max = sum
-            end
-        end
-        it = inc(it, #m)
-        if not it then
-            break
+    local max = res[1]
+    for j = 2, #res do
+        if res[j] > max then
+            max = res[j]
         end
     end
+   return max
 end
 
-problem345(BIGMATRIX)
+print(msum(LILMATRIX))
